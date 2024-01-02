947baf79a459898b07cd44441398996bf1133551
佟大印 <tongdayin@udesk.cn>
2024-01-02 20:07:13 +0800

    search and pagination

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -24,8 +24,8 @@ const links = [
 export default function NavLinks() {
   const pathname = usePathname();
 
-    console.log('pathname', pathname);
-    
+  console.log('pathname', pathname);
+
   return (
     <>
       {links.map((link) => {
